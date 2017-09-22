9df7e80aa44b557512bd564c679e99ee30e349c0
LETS-STAR\user <ifhange@gmail.com>
2017-09-23 02:51:54 +0800

    0923 更新首頁(粗略版)、更改path、刪除舊檔案

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,70 +1,46 @@
 import React from "react";
-import RoomList from "./RoomList";
-import socket from "../socket";
+import Background from "./special-field/Background";
 
 // socket.emit("id");
 class Index extends React.Component {
     constructor(props) {
         super(props);
-        this.localUserID = "";
+        this.onClick_handleCreateRoom = this.onClick_handleCreateRoom.bind(this);
     }
 
     componentDidMount() {
-        socket.on("success", msg => {
-            this.localUserID = msg;
-        });
     }
 
-    handleEnter(e) {
-        if (e.charCode == 13 && !this.refs.userName.value) {
-            event.preventDefault();
-            let r = confirm("你沒有輸入名字喔，我們將會給你一組亂碼好嗎?");
-            if (r == true) {
-                this.refs.userName.value = "Hi! " + this.localUserID;
-            }
-            //socket.emit('setFakeName', this.refs.Username.value);
-            //this.roomName = this.refs.roomnum.value;
-        } else if (e.charCode == 13 && this.refs.userName.value) {
-            event.preventDefault();
-            if (this.refs.userName.value.search("Hi!") == -1) {
-                this.refs.userName.value = "Hi! " + this.refs.userName.value;
-            } else {
-                return false;
-            }
-        }
+    onClick_handleCreateRoom() {
+        //按下建立房間後的事件
     }
+
 
     render() {
         return (
             <div className="container">
-                <div className="Index">
-                    <div className="logo">
-                        <div id="in">
-                            <img id="Indexlogo" src="../img/index_logo.png" />
-                        </div>
+                <div className="index">
+                    <img className="indexlogo" src="./img/index_logo2.png" />
+                    <br />
+                    <div className="indexName">
+                        名字:
+                        <input className="indexinput" type="text" />
+                        <span className="focus-border"></span>
                     </div>
-                    <div className="inputName">
-                        <span className="input input--isao">
-                            <input
-                                ref="userName"
-                                onKeyPress={(e)=>{this.handleEnter(e)}}
-                                className="input__field input__field--isao"
-                                type="text"
-                                id="input-38"
-                            />
-                            <label
-                                className="input__label input__label--isao"
-                                for="input-38"
-                                data-content="請輸入你的名字"
-                            >
-                                <span className="input__label-content input__label-content--isao">
-                                    請輸入你的名字
-                                </span>
-                            </label>
-                        </span>
+                    <br />
+                    <div className="indexMeeting">
+                        房間名稱:
+                        <input className="indexinput" type="text" />
+                        <span className="focus-border"></span>
+                    </div>
+                    <div
+                        className="addRoom"
+                        onClick={this.onClick_handleCreateRoom}
+                    >
+                        建立房間
                     </div>
                 </div>
-                <RoomList />
+                <Background />
             </div>
         );
     }
