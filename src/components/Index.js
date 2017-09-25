03a53fafdcb527612c2c88c27ff941e427a5e043
Wei-Chun <weichun0911@gmail.com>
2017-09-25 13:12:50 +0800

    新增首頁功能>按下enter會自動判斷

--- src/components/Index.js
+++ src/components/Index.js
@@ -18,7 +18,43 @@ class Index extends React.Component {
         );
     }
 
+    componentWillMount() {
+        document.addEventListener("keydown", this.onKeyDown.bind(this));
+    }
+
     componentDidMount() {}
+
+    onKeyDown(e) {
+        console.log(document.activeElement);
+        switch (e.keyCode) {
+            case 13:
+                if (!this.refs.roomName.value) {
+                    this.refs.roomName.focus();
+                    return;
+                }
+                if (!this.refs.userName.value) {
+                    this.refs.userName.focus();
+                    return;
+                }
+                if (this.state.userName && this.state.roomName) {
+                    if (
+                        document.activeElement == this.refs.roomName ||
+                        document.activeElement == this.refs.userName
+                    ) {
+                        document.activeElement.blur();
+                        return;
+                    }
+
+                    if (document.activeElement == document.body) {
+                        this.onClick_handleCreateRoom();
+                        return;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
     onChange_userName(e) {
         this.setState({ userName: e.target.value });
@@ -30,7 +66,7 @@ class Index extends React.Component {
         //按下建立房間後的事件
         this.props.dispatch(setUserName(this.state.userName));
         //做好名字之後>進到房間裡
-        this.props.history.push("/meeting#" + this.state.roomName)
+        this.props.history.push("/meeting#" + this.state.roomName);
     }
 
     render() {
@@ -42,6 +78,8 @@ class Index extends React.Component {
                     <div className="indexName">
                         名字:
                         <input
+                            autoFocus={true}
+                            ref="userName"
                             className="indexinput"
                             type="text"
                             onChange={e => {
@@ -54,6 +92,7 @@ class Index extends React.Component {
                     <div className="indexMeeting">
                         房間名稱:
                         <input
+                            ref="roomName"
                             className="indexinput"
                             type="text"
                             onChange={e => {
