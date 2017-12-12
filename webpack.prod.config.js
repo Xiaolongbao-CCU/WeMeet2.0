8ed1eba182910aef789d8616f85e8d8de1df8e4c
Wei-Chun <weichun0911@gmail.com>
2017-12-13 00:08:02 +0800

    update webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,37 +1,50 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 module.exports = {
-    context: path.resolve(__dirname, "./src"),
-    entry: ["./main.js"],
-    output: {
-        path: path.resolve(__dirname, "./public/"),
-        filename: "bundle.js",
-        publicPath: "/"
+  context: path.resolve(__dirname, './src'),
+  entry: ['./main.jsx'],
+  output: {
+    path: path.resolve(__dirname, './public/'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react'],
+        },
+      }],
+      exclude: /node_modules/,
     },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["es2015", "react"]
-                        }
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader?modules"]
-            }
-        ]
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
     },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": '"production"'
-        })
-    ]
+    {
+      test: /\.scss$/,
+      use: extractPlugin.extract({
+        use: ['css-loader', 'sass-loader'],
+      }),
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+    extractPlugin,
+  ],
 };
